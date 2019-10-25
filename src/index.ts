e4d97ef73cd7c790494055b904a0a2313602861e
Pontus Östlund <ponost@roxen.com>
2019-10-25 08:35:10 +0200

    Expose rss2 as its own namespace

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as types from './lib/types'
+import * as Rss2 from './rss2'
 export { Feed } from './lib/feed'
 export { Options } from './lib/options'
-export { types }
+export { types, Rss2 }
